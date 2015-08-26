15b4140eda12f966631a231f5f4c4d7c7405ed5f
Chad Reddick <chad_reddick2008@yahoo.com>
2015-08-26 14:08:41 -0500

    Added Project Files

---
+++ TitanBots/Source/TitanBots/TitanBots.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "TitanBots.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TitanBots, "TitanBots" );
+ 
---
+++ TitanBots/Source/TitanBots/TitanBotsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "TitanBotsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATitanBotsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ATitanBotsGameMode();
+};
+
+
+
